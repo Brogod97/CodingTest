e3a62f098454b8fac3ac568c5f3b851dbb923057
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-02-28 20:16:07 +0900

    Update 짝수의 합.py

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -13,3 +13,9 @@ def solution(n):
 
 # range() : 연속된 숫자들의 시퀀스를 생성하는 데 사용
 # range([start], stop[, step]) : [시작], 종료, [간격]을 입력
+
+## 참고 답안
+# return sum([i for i in range(2, n + 1, 2)])
+# range는 이미 iterable 객체이기 때문에 list comprehension으로 다시 풀어 쓸 필요 없이 바로 sum 함수에 넣을 수 있음
+
+# return sum(filter(lambda v: v % 2 == 0, [_ for _ in range(n+1)]))
