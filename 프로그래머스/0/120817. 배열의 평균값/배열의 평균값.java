e5830394ea4dfc0446b8bc4503df83cac9434306
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-02-29 17:16:23 +0900

    Update 배열의 평균값.java

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -11,3 +11,9 @@ class Solution {
         return sum / size;
     }
 }
+
+// 다른 풀이
+// return Arrays.stream(numbers).average().orElse(0);
+// Arrays.stream(numbers) : numbers 배열을 스트림으로 변환
+// .average() : 스트림 요소들의 평균 값 반환
+// .orElse(0) : 주어진 값이 null이 아닌 경우 주어진 값을 되돌려주고 null인 경우 인수(0)을 돌려줌
