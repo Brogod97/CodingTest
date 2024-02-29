99133fc37a8f4f9d4f28b32a85c2af953c01e3f9
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-02-29 17:13:52 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,13 @@
+import java.util.stream.*;
+
+class Solution {
+    public double solution(int[] numbers) {
+        double sum = 0;
+        int size = numbers.length;
+        
+        for(int i = 0; i < size; i++) {
+            sum += numbers[i];
+        }
+        return sum / size;
+    }
+}
