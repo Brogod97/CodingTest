9bb8b79950f185351ec2a1fba795a79350caffd0
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-02-29 17:24:51 +0900

    Update 배열의 평균값.py

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.py
@@ -4,3 +4,7 @@ def solution(numbers):
 
 # sum(iterable, start=0) : 합계
 # len(sequence) : 시퀀스나 컬렉션의 길이 계산
+
+# c.f. numpy (수치 해석, 통계 관련 기능을 구현한다고 할 때 가장 기본이 되는 모듈)
+# np.mean() : 산술 평균 계산
+# np.average() : 가중치 옵션 적용 가능 (가중 평균 가능)
