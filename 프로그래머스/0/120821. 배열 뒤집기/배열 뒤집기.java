50c91f84132f9eff7a2cde633daa4c4648dfc727
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-01 19:20:37 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,9 +1,10 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
+        int size = num_list.length;
+        int[] answer = new int[size];
         
-        for(int i = 0; i < num_list.length; i++) {
-            answer[num_list.length - 1 - i] = num_list[i];
+        for(int i = 0; i < size; i++) {
+            answer[size - 1 - i] = num_list[i];
         }
         
         return answer;
