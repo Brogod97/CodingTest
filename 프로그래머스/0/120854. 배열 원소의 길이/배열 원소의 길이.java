87ea0d20dacc580714502c73dc5bb85ef7a5060b
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-02 14:07:53 +0900

    Update 배열 원소의 길이.java

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -10,3 +10,13 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return Arrays.stream(strList).mapToInt(String::length).toArray();
+// Arrays.stream(strList): strList를 스트림으로 변환
+/* .mapToInt(String::length)
+    - mapToInt(): 스트림의 각 요소를 특정한 방식으로 변환 후 정수 스트림 생성
+
+    - 메소드 레퍼런스 (클래스::메소드) : 람다식과 같은 기능, 다만 메소드를 참조하여 호출
+    -- String::length == String객체의 length() 호출
+*/
