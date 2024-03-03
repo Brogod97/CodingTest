d35a57a30cab165e6e41420fb0f47b72c65e52d4
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-03 11:40:28 +0900

    Update 문자열 뒤집기.py

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,2 +1,11 @@
 def solution(my_string):
-    return "".join(reversed(my_string))
+    return ''.join(reversed(my_string))
+
+# 다른 풀이
+# 1. 문자열 슬라이싱
+return my_string[::-1]
+
+#2. for문
+for i in range(len(my_string)-1, -1, -1) : 
+    answer += my_string[i]
+# 시작값: len(my_string)-1(마지막 인덱스부터), 종료: -1(0까지 역순으로 반복), 간격: -1
