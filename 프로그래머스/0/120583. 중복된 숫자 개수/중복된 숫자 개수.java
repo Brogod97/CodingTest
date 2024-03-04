cc93a7c08e61510eec45e16e27a920e38b8b423e
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-04 14:36:31 +0900

    Update 중복된 숫자 개수.java

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -13,3 +13,8 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+ return (int) Arrays.stream(array).filter(e -> e == n).count();
+// .filter() : 스트림에서 주어진 조건을 만족하는 요소만을 선택하여 새로운 스트림을 생성
+// .count() : 갯수 반환 (반환형 long)
