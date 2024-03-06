135f5c98e2719bc07261e810bf9c057b3b67ccdc
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-06 17:07:50 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.28 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, String letter) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(String str : my_string.split("")) {
+            if(!str.equals(letter)) {
+                sb.append(str);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
