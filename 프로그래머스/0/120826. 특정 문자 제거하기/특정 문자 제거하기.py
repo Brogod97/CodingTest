b37cfe7611616d9e2b9c920a68243192e7bcf715
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-06 17:15:20 +0900

    Update 특정 문자 제거하기.py

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,2 +1,15 @@
 def solution(my_string, letter):
-    return my_string.replace(letter, "")
+    return my_string.replace(letter, '')
+
+# .replace(타겟, 변경할 문자 [, 변경할 횟수]) : 문자열을 변경하는 함수
+# 횟수 생략 시 문자열 전체를 변경
+
+# 다른 답안 1
+answer = ''
+for string in my_string:
+    if string != letter:
+        answer += string
+return answer
+
+# 다른 답안 2
+return ''.join([c for c in my_string if c != letter])
