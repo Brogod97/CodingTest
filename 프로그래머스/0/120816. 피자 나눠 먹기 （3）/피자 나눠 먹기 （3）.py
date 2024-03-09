04ae384669cdee097e232fb1c4a5a14f33a004ab
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-09 19:32:46 +0900

    Update 피자 나눠 먹기 （3）.py

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.py
@@ -1,2 +1,12 @@
 def solution(slice, n):
     return n // slice + 1 if n % slice != 0 else n // slice
+
+# 다른 풀이
+return ((n - 1) // slice) + 1
+
+# 다른 풀이
+d, m = divmod(n, slice)
+return d + int(m != 0)
+
+# 다른 풀이
+return ceil(n/slice)
