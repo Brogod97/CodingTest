49d4b120f1006c5034b752840bc2b760b66bf709
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-11 12:16:44 +0900

    Update 대소문자 바꿔서 출력하기.py

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -8,3 +8,10 @@ for i in str:
         answer += i.upper()
     
 print(answer)
+
+# 다른 풀이
+print(input().swapcase())
+# swapcase() : 대소문자 상호 전환
+# upper() : 대문자로 변환
+# lower() : 소문자로 변환
+# title() : 각 단어의 앞글자만 대문자로 변환
