a8a5258095d5baa4cc3782c7d9f7d95e14c82845
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-11 12:04:20 +0900

    Update 대소문자 바꿔서 출력하기.java

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -14,3 +14,18 @@ public class Solution {
         }
     }
 }
+
+// 출력구문을 두번 작성하지 않고, String answer 변수를 선언하여 관리하는 방법도 괜찮을듯
+
+// 다른 풀이
+for(int i=0; i<a.length(); i++) {
+    char c = a.charAt(i);
+    if(Character.isUpperCase(c)) {
+        System.out.print((char)(c+32));
+    }
+    else {
+        System.out.print((char)(c-32));
+    }
+}
+// 32의 의미 : ASCII 코드에서 대문자 / 소문자 간 차이
+// 예시 - A = 65, a = 97
