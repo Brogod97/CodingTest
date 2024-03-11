c0ee3e6db2ab209c72603f73d56847d57de5247c
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-11 11:56:49 +0900

    Update 문자열 반복해서 출력하기.java

--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -11,3 +11,18 @@ public class Solution {
         }
     }
 }
+
+// 다른 풀이
+System.out.println(str.repeat(n));
+// String.repeat(n)
+// - Java 11에서 새로 추가된 Stirng 메서드, 문자열을 파라미터의 주어진 횟수만큼 반복
+// - 파라미터가 0일 경우, 빈 문자열 반환
+// - 음수일 경우, IllegalArgumentExceptionthrow 반환
+// - 1일 경우, 문자열 그대로 반환
+
+// 다른 풀이
+StringBuilder sb = new StringBuilder();
+for(int i=0; i<n; i++){
+    sb.append(str);
+}
+System.out.println(sb.toString());
