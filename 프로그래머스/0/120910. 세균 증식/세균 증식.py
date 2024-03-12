5694064bc215a43fb3903f9718b3ef48bb6f309c
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-12 22:58:26 +0900

    Update 세균 증식.py

--- 프로그래머스/0/120910. 세균 증식/세균 증식.py
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.py
@@ -7,3 +7,16 @@ def solution(n, t):
         i += 1
         
     return answer
+
+# 다른 풀이
+return n << t
+# 시프트 연산자 : 비트의 위치 이동
+# << (왼쪽), >> (오른쪽)
+
+# 다른 풀이
+return 2**t * n
+# ** : 거듭 제곱 연산자
+
+# 다른 풀이
+for i in range(t) :
+    n *= 2
