024f7fa9dfdea20b35230403d7af208fe0587355
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-13 13:19:07 +0900

    Update 아이스 아메리카노.py

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -5,3 +5,10 @@ def solution(money):
     answer.append(money%5500)
     
     return answer
+
+# 다른 풀이
+return [money // 5500, money % 5500]
+
+# 다른 풀이
+return divmod(money, 5500)
+# divmod() : 몫과 나머지를 동시에 구할 때 사용
