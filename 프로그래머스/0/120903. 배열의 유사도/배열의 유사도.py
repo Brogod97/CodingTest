1c711ec79f59ba8e9b491b5ae8b09f9ae7a756a2
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-13 14:21:17 +0900

    Update 배열의 유사도.py

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -6,3 +6,10 @@ def solution(s1, s2):
             answer += 1
     
     return answer
+
+# 다른 풀이
+return len(set(s1)&set(s2));
+# 1. set(S1), set(s2) : Set 자료구조로 변환
+# - set : 중복된 값을 허용하지 않는 자료구조
+# 2. & : 교집합 획득 (set.intersection과 동일)
+# 3. len() : 길이 반환
