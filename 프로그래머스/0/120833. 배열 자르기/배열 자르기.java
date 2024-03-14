f0aead61088826c52d82a6684f2647a532efb992
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-14 23:12:21 +0900

    Update 배열 자르기.java

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -5,3 +5,18 @@ class Solution {
         return Arrays.copyOfRange(numbers, num1, num2 + 1);
     }
 }
+
+// 다른 풀이
+int[] answer = new int[num2 - num1 + 1];
+
+for (int i = num1; i <= num2; i++) {
+    answer[i -num1] = numbers[i];
+}
+
+return answer;
+
+// 다른 풀이
+return IntStream.rangeClosed(num1, num2).map(i -> numbers[i]).toArray();
+
+// 다른 풀이
+return Arrays.stream(numbers, num1, num2+1).toArray();
