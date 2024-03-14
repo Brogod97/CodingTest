db69e81ad50407d48b8282b8ce51c6faeaf256a5
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-14 23:18:15 +0900

    Update 배열 자르기.py

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.py
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.py
@@ -4,3 +4,9 @@ def solution(numbers, num1, num2):
 # slice - sequence[start:stop:step]
 # 시퀀스(Sequence) 자료형에서 부분 집합을 추출하는 방법
 # stop 값은 결과에 포함되지 않음
+
+# 다른 풀이
+answer=[]
+for i in range(num1,num2+1):
+    answer.append(numbers[i])
+return answer
