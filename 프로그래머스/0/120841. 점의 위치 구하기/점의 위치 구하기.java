c7a75d563e7fd0a06919151d970655a311e90930
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-15 22:59:01 +0900

    Update 점의 위치 구하기.java

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -13,3 +13,16 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+int xParam = dot[0] < 0 ? 1 : 0;
+int yParam = dot[1] < 0 ? 2 : 0;
+
+return 1 + ((xParam | yParam) ^ (yParam >> 1));
+// 비트 연산을 이용한 풀이
+// (xParam | yParam) : 두 변수 중 하나라도 해당 비트가 설정되어 있으면 결과 설정 
+// -- xParam이 1이고, yParam이 2라면 3 반환
+// (yParam >> 1) : yParam을 오른쪽으로 1비트 시프트한 값
+// -- yParam이 2일 경우 1 반환
+// ^ : XOR 연산, 좌 우측 연산의 값이 다른 경우에만 1 반환, 같으면 0 반환
+// 1 + : 마지막으로 결과에 1을 더함
