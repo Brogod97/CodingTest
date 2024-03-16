6335fcc673b829c2c0f46e25e7e63fa18229bc11
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-16 22:36:20 +0900

    Update 배열 두배 만들기.py

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,2 +1,16 @@
 def solution(numbers):
     return list(map(lambda i : i * 2, numbers))
+
+# 참고
+# map > list comprehension > map + lambda > for loop 으로 성능이 좋음
+
+# 다른 풀이
+# 리스트 컴프리헨션
+return [num*2 for num in numbers]
+
+# 다른 풀이
+answer = []
+for num in numbers:
+    answer.append(num * 2)
+return answer
+
