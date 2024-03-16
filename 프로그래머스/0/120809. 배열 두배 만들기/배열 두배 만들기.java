680bc08aec53e0f6c8c1e4e515098627759c212f
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-16 22:30:07 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.70 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,11 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        
+        return IntStream.of(numbers)
+                        .map(i -> i * 2)
+                        .toArray();
+    }
+}
