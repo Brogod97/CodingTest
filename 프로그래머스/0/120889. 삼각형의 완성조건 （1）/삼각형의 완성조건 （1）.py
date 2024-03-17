1d8ad81d9271f9377148a2fa0f1278863f6bd398
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-17 14:19:35 +0900

    Update 삼각형의 완성조건 （1）.py

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -2,3 +2,18 @@ def solution(sides):
     sides.sort()
     
     return 1 if sides[0] + sides[1] > sides[2] else 2    
+
+# 참고
+# sort() 와 sorted()의 차이
+# sort() : 기존 리스트 정렬
+# sorted() : 정렬된 새로운 리스트 반환
+
+# 다른 풀이
+return 1 if max(sides) < (sum(sides) - max(sides)) else 2
+
+# 다른 풀이
+c = max(sides)
+sides.remove(c)
+a = sum(sides)
+
+return 1 if c < a else 2
