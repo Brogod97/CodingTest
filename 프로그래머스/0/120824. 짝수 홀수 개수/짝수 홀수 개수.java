89f12cce751bfcedff64d3f5b15bd7e3603fea93
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-18 11:38:48 +0900

    Update 짝수 홀수 개수.java

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -15,3 +15,14 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+ int[] answer = new int[2];
+
+for(int i = 0; i < num_list.length; i++)
+    answer[num_list[i] % 2]++;
+
+return answer;
+
+// 다른 풀이
+return IntStream.of((int) Arrays.stream(numList).filter(i -> i % 2 == 0).count(), (int) Arrays.stream(numList).filter(i -> i % 2 == 1).count()).toArray();
