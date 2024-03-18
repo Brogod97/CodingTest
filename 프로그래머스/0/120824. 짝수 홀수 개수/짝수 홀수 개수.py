3bf2cf701c672b88b9b28e7a7035bc0d0dbfc17d
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-18 11:58:50 +0900

    Update 짝수 홀수 개수.py

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,7 +1,6 @@
 def solution(num_list):
     answer = []
-    even = 0
-    odd = 0
+    even, odd = 0, 0
     
     for num in num_list:
             if(num % 2 == 0):
@@ -13,3 +12,9 @@ def solution(num_list):
     answer.append(odd)
     
     return answer
+
+# 다른 풀이
+answer = [0,0]
+for n in num_list:
+    answer[n%2]+=1
+return answer
