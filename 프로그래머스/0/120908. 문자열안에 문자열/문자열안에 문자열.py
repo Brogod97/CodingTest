fb379824dca97ffc084806220ff4e5827bbd4017
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-19 22:03:59 +0900

    Update 문자열안에 문자열.py

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.py
@@ -1,2 +1,21 @@
 def solution(str1, str2):
     return 1 if str2 in str1 else 2
+
+# str.find("abc") : 찾고자하는 문자열이 존재할 경우, 시작 인덱스 값을 리턴 (없을 경우 -1 리턴)
+# in, not in : 결과값이 있으면 True, 없으면 False 반환
+
+# 다른 풀이
+return 1 + int(str2 not in str1)
+
+# 다른 풀이
+if str1.count(str2):
+    return 1
+else:
+    return 2
+
+# 다른 풀이
+answer = str1.split(str2)
+if len(answer) == 1:
+    return 2
+else :
+    return 1
