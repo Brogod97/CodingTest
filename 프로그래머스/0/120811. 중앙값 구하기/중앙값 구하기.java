c698600330b5bbe766b078468a6590b00e888ad8
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-22 23:52:53 +0900

    Update 중앙값 구하기.java

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -8,3 +8,20 @@ class Solution {
         return array[array.length / 2];
     }
 }
+
+// 다른 풀이
+ int answer = 0;
+
+for (int i = 0; i < array.length; i++) {
+    for (int j = i; j < array.length; j++) {
+        if (array[j] < array[i]) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+
+answer = array[array.length / 2];
+
+return answer;
