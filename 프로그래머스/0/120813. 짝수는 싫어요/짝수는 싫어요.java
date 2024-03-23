42fa467befc62e82438516190705bc77a7acaf21
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-23 21:22:39 +0900

    Update 짝수는 싫어요.java

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -8,3 +8,19 @@ class Solution {
                         .toArray();
     }
 }
+
+// 다른 풀이
+int cnt = 0;
+
+if(n % 2 == 0) cnt = n/2;
+else cnt = n/2 + 1;
+
+int[] result = new int[cnt];
+int num = 1;
+
+for(int i=0; i<result.length; i++){
+    result[i] = num;
+    num = num + 2;
+}
+
+return result;
