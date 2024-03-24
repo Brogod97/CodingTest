9a763fd4e99da0548cc9064ba96c637627c74360
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-24 15:17:15 +0900

    Update 숨어있는 숫자의 덧셈 （1）.py

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,3 +1,24 @@
 def solution(my_string):
     
     return sum([int(x) for x in list(my_string) if x.isdigit()])
+
+# 다른 풀이
+answer = 0
+for x in list(my_string):
+    if(x.isdigit()):
+        answer += int(x)
+
+return answer
+
+# 다른 풀이
+answer = 0
+for i in my_string:
+    try:
+        answer = answer + int(i)
+    except:
+        pass
+
+return answer
+
+# 다른 풀이
+return sum(map(int, filter(lambda x: x.isdigit(), my_string)))
