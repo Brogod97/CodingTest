00dc219662dee8c207677f28ea5e60b02cf49f1d
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-24 15:04:59 +0900

    Update 숨어있는 숫자의 덧셈 （1）.java

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -10,3 +10,31 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return myString.chars()
+                .mapToObj(i -> (char) i)
+                .filter(Character::isDigit) // isDigit() : char 값이 숫자 인지 여부를 판단하여 boolean으로 반환
+                .map(String::valueOf)       // String.valueOf() : String으로 변환 
+                .mapToInt(Integer::valueOf) // Integer.valueOf() : 문자열을 정수로 변환 (Integer 타입으로 반환)
+                .sum();
+
+// 다른 풀이
+int answer = 0;
+String str = my_string.replaceAll("[^0-9]","");
+
+for(char ch : str.toCharArray()) {
+    answer += Character.getNumericValue(ch); // 숫자 형태 char -> int로 변환
+}
+
+return answer;
+
+// 다른 풀이
+int answer = 0;
+
+for(char c : my_string.toCharArray()){
+    if(c >= '0' && c <= '9'){
+        answer += c - '0';
+    }
+}
+return answer;
