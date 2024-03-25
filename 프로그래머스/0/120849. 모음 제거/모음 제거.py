b46125a351a756ed2b99a0affc069793c3e09641
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-25 16:34:54 +0900

    Update 모음 제거.py

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -5,3 +5,12 @@ def solution(my_string):
     reg = re.compile("[^aeiou]")
     
     return ''.join(reg.findall(my_string))
+
+# 다른 풀이
+return "".join([i for i in my_string if not(i in "aeiou")])
+
+# 다른 풀이
+vowels = ['a','e','i','o','u']
+for vowel in vowels:
+    my_string = my_string.replace(vowel, '')
+return my_string
