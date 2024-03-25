a1059bd44774e9164986c8ac310618d0061b8981
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-25 16:25:24 +0900

    Update 모음 제거.java

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -6,3 +6,12 @@ class Solution {
         return my_string.replaceAll("[aeiou]", "");
     }
 }
+
+// 다른 풀이
+String[] vowels = new String[]{"a", "e", "i", "o", "u"};
+for(String vowel : vowels){
+    if(my_string.contains(vowel)){
+        my_string = my_string.replace(vowel, "");
+    }
+}
+return my_string;
