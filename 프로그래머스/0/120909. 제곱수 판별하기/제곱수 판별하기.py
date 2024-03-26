7b293aabd21ccd4ce3859b7045593e4423f706e6
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-26 17:39:37 +0900

    Update 제곱수 판별하기.py

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.py
@@ -3,3 +3,13 @@ import math
 def solution(n):    
     
     return 1 if math.sqrt(n) % 1 == 0 else 2
+
+# 다른 풀이
+return 1 if (n ** 0.5).is_integer() else 2
+# .is_integer() : 정수면 true, 아니면 false
+
+# 다른 풀이
+if n**(1/2) == int(n**(1/2)) :
+    return 1
+else :
+    return 2
