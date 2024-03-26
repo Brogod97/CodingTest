75d4d39a54e12400128d3e732597c0c636a8af83
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-26 17:35:58 +0900

    Update 제곱수 판별하기.java

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,8 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         
         return Math.sqrt(n) % 1 == 0 ? 1 : 2;
     }
 }
+
+// 다른 풀이
+if (n % Math.sqrt(n) == 0) {
+    return 1;
+} else {
+    return 2;
+}
+
+// 다른 풀이
