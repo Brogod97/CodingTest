873a91375ef9dd5db7ac8247e4c97e1854a5de3f
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-27 19:58:29 +0900

    Update n의 배수 고르기.java

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -8,3 +8,22 @@ class Solution {
                         .toArray();
     }
 }
+
+// 다른 풀이
+int count = 0;
+for(int i : numlist){
+    if(i%n==0){
+        count++;
+    }
+}
+
+int[] answer = new int[count];
+int idx = 0;
+for(int i : numlist){
+    if(i%n==0){
+        answer[idx]=i;
+        idx++;
+    }
+}
+
+return answer;
