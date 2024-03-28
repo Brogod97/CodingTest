a0d3cae312c1c25505413d2f1ad9bc1d8385e4f9
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-28 12:49:50 +0900

    Update 가위 바위 보.py

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -10,3 +10,12 @@ def solution(rsp):
             answer += "2";
     
     return answer
+
+# 다른 풀이
+d = {'0':'5','2':'0','5':'2'}
+return ''.join(d[i] for i in rsp)
+
+return rsp.translate(str.maketrans('025', '502'))
+# str.translate() : 문자열의 여러 문자를 한 번에 치환
+# str.maketrans() : 문자열을 다른 문자열로 매핑하는 테이블 생성
+# 즉, maketrans를 이용해 '0'은 '5'로, '2'는 '0'으로, '5'는 '2'로 변환 후 translate를 통해 rsp 문자열에 대해 앞서 변환한 테이블을 적용
