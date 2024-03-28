957282b3e699feba9466aba5bae3e1e86cbfde59
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-28 12:35:04 +0900

    Update 가위 바위 보.java

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -2,14 +2,31 @@ class Solution {
     public String solution(String rsp) {
         String answer = "";
         
-        for(char i : rsp.toCharArray()) {
-            switch(i) {
-                case '0' -> answer += "5";
-                case '2' -> answer += "0";
-                case '5' -> answer += "2";
+        for(String str : rsp.split("")) {
+            switch(str) {
+                case "0" -> answer += "5";
+                case "2" -> answer += "0";
+                case "5" -> answer += "2";
             }
         }
         
         return answer;
     }
 }
+
+// 다른 풀이
+return Arrays.stream(rsp.split("")).map(s -> s.equals("2") ? "0" : s.equals("0") ? "5" : "2").collect(Collectors.joining());
+
+// 다른 풀이
+Map<String, String> winNumbers = new HashMap<>();
+winNumbers.put("2", "0");
+winNumbers.put("0", "5");
+winNumbers.put("5", "2");
+
+StringBuilder answer = new StringBuilder();
+
+for (int i = 0; i < rsp.length(); i++) {
+    answer.append(winNumbers.get(rsp.substring(i, i+1)));
+}
+
+return answer.toString();
