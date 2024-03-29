95718ea38d3afb958fe91cef7dd3e07b2fe6b1a7
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-29 16:51:49 +0900

    Update 글자 이어 붙여 문자열 만들기.java

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -10,3 +10,26 @@ class Solution {
         return sb.toString();
     }
 }
+
+// 다른 풀이
+String answer = "";
+        
+for(int i = 0; i < index_list.length; i++) {
+    int index = index_list[i];
+    answer += my_string.charAt(index);
+}
+
+return answer;
+
+// 다른 풀이
+return Arrays.stream(indexList)
+             .mapToObj(operand -> String.valueOf(myString.charAt(operand)))
+             .collect(Collectors.joining());
+
+// 다른 풀이
+String answer = "";
+String[] str = my_string.split("");
+for(int index : index_list) {
+    answer += str[index];
+}
+return answer;
