c45567102d0d9e5072e68530da33e718b63d2d59
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-30 23:23:10 +0900

    Update n개 간격의 원소들.py

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.py
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.py
@@ -1,3 +1,15 @@
 def solution(num_list, n):
     
     return num_list[::n]
+
+# 슬라이싱: 리스트[start:stop:step]
+#    - start: 시작 위치(포함 O), 생략 시 처음부터
+#    - stop: 종료 위치(포함 X), 생략 시 끝까지
+#    - step: 건너뛸 간격, 생략시 1
+
+# 다른 풀이
+return num_list[0 : len(num_list) : n]
+
+# 다른 풀이
+return [num_list[i] for i in range(0, len(num_list), n)]
+# range(start, stop, step): 슬라이싱의 인자 설명과 동일 
