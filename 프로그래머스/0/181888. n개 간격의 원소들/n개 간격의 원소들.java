699cafcce741735973a7998ce3a28c5ea2179881
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-30 22:51:48 +0900

    Update n개 간격의 원소들.java

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -15,3 +15,12 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return IntStream.iterate(0, i -> i < numList.length, i -> i + n)
+                .map(i -> numList[i])
+                .toArray();
+// .iterate() : 초기 값과 조건, 그리고 요소를 생성하는 함수를 인자로 받음
+//    - 초기값 : 0
+//    - 조건 : i -> i < numList.length
+//    - 값 변경 함수 : i -> i + n
