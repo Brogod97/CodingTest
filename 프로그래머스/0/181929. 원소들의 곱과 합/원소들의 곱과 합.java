fc97224d84b1f0ff976bbdbb2980e0264a31c90f
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-03-31 10:50:07 +0900

    Update 원소들의 곱과 합.java

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -11,3 +11,8 @@ class Solution {
         return mul < sum * sum ? 1 : 0;
     }
 }
+
+// 다른 풀이
+return (Arrays.stream(numList).reduce((acc, i) -> acc * i).getAsInt() < Math.pow(Arrays.stream(numList).sum(), 2)) ? 1 : 0;
+// reduce() : 스트림의 요소를 줄이고(reduce) 하나의 값으로 결합하는 데 사용
+// Math.pow(밑, 지수)
