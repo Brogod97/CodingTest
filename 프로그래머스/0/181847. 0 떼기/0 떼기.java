5434effd668f1ae51e4db3cb68ea7168b7d1728b
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-03 21:53:57 +0900

    Update 0 떼기.java

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -11,3 +11,9 @@ class Solution {
         return n_str;
     }
 }
+
+// 다른 풀이
+return ""+Integer.parseInt(n_str);
+// parseInt를 사용하면 좌측에 붙은 0은 제거됨
+//    - 문자열을 char 단위로 순회하며 *10 하여 자릿수 올림 수행 후 더함
+// https://www.inflearn.com/questions/415221/parseint%EA%B0%80-0%EC%9D%84-%EC%A0%9C%EC%99%B8%ED%95%98%EB%8A%94-%EC%9D%B4%EC%9C%A0
