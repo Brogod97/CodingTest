8735fb8efd178f1d7d62cd75222010b38168657f
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-03 22:02:42 +0900

    Update 0 떼기.py

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -8,3 +8,18 @@ def solution(n_str):
             break
     
     return n_str[idx::]
+
+# 다른 풀이
+return n_str.lstrip('0')
+# lstrip() : 왼쪽 공백 또는 인자와 일치하는 문자열 제거
+# rstrip() : 오른쪽 ~
+# strip() : 양쪽 ~
+
+# 다른 풀이
+return str(int(n_str))
+# int로 변환 과정에서 0으로 시작되는 부분은 제거
+
+# 다른 풀이
+for i in range(len(n_str)):
+    if n_str[i] != '0':
+        return n_str[i:]
