2165b18acf791ddf14b4736b0a540814c8010f96
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-08 22:16:34 +0900

    Update 배열에서 문자열 대소문자 변환하기.py

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.py
@@ -10,3 +10,9 @@ def solution(strArr):
         idx += 1
     
     return answer
+
+# 다른 풀이
+return [s.lower() if i % 2 == 0 else s.upper() for i, s in enumerate(strArr)]
+# enumerate(): 인덱스와 값의 쌍으로 이루어진 이터러블을 생성
+#     - 0: "AbC"
+#     - 1: "aBc"
