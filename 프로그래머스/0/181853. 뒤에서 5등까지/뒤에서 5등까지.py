64ef5b56dae844b1e87c6b2601c61f0cc4c417c2
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-10 19:58:10 +0900

    Update 뒤에서 5등까지.py

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.py
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.py
@@ -2,3 +2,15 @@ def solution(num_list):
     num_list.sort()
     
     return num_list[:5]
+
+# 다른 풀이
+return sorted(num_list)[:5]
+
+# 다른 풀이
+answer = []
+
+for i in range(5):
+    answer.append(min(num_list))
+    num_list.remove(min(num_list))
+    
+return answer
