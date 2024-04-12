d9a390ab2bb626d6b660e949ad06e2725dd984ac
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-12 22:41:15 +0900

    Update x 사이의 개수.py

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,3 +1,9 @@
 def solution(myString):
 
     return [len(str) for str in myString.split('x')]
+
+# 다른 풀이
+return list(map(lambda x: len(x), myString.split('x')))
+# map(function, iterable, ...) : 주어진 함수를 시퀀스(리스트, 튜플 등)의 각 요소에 적용하여 새로운 시퀀스를 반환
+#     - function : 시퀀스의 각 요소에 적용할 함수
+#     - iterable : 입력 시퀀스
