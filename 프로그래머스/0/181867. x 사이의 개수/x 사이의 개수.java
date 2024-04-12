9e245e7029d87b1a2e6ab80df7ff0eedc885613f
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-12 22:36:51 +0900

    Update x 사이의 개수.java

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -5,7 +5,13 @@ class Solution {
     public int[] solution(String myString) {
         
         return Arrays.stream(myString.split("x", myString.length()))
-              .mapToInt(str -> str.length())
+              .mapToInt(str -> str.length()) // .mapToInt(String::length)
               .toArray();
     }
 }
+
+// 다른 풀이
+return Arrays.stream(myString.split("x", -1)).map(String::length).toArray(Integer[]::new);
+// split(String regex, int limit) : limit을 -1로 전달하면 분할된 문자열에 제한 없이 모두 배열에 포함
+// mapToInt() : IntStream 반환
+// map() : Stream<Integer> 반
