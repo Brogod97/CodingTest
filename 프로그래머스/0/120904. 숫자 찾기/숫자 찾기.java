cd1d9e91aa781393f9e8197f8140bd9ba8daaf33
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-13 12:23:34 +0900

    Update 숫자 찾기.java

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -12,3 +12,15 @@ class Solution {
         return -1;
     }
 }
+
+// 다른 풀이
+return ("-" + num).indexOf(String.valueOf(k));
+// "-" + num : "-"는 인덱스를 1부터 카운트하게 만들기 위함 ("-" 자체는 큰 의미가 없음, 부호 X)
+// String.indexOf(String str) : 문자열(String)이나 배열(Array)에서 지정된 요소가 처음으로 나타나는 인덱스를 반환 (없으면 -1)
+
+// 다른 풀이
+String numStr = String.valueOf(num);
+String kStr = String.valueOf(k);
+
+int answer = numStr.indexOf(kStr);
+return answer < 0 ? -1 : answer + 1 ;
