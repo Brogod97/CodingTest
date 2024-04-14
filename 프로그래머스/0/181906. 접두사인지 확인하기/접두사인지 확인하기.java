e8f05aa8b27f5844318b679534302530e4097e3e
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-14 17:02:47 +0900

    Update 접두사인지 확인하기.java

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -5,3 +5,23 @@ class Solution {
         return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
+
+// 다른 풀이
+int answer = 0;
+int k=1;
+String[] arr=new String[my_string.length()];
+for(int i=0;i<my_string.length();i++){
+    arr[i]=my_string.substring(0,k);
+    k++;
+}
+for(int i=0;i<my_string.length();i++){
+    if(arr[i].equals(is_prefix)){
+        answer=1;
+    }
+}
+return answer;
+
+// 다른 풀이
+return IntStream.rangeClosed(0, myString.length())
+                .mapToObj(i -> myString.substring(0, i))
+                .anyMatch(s -> s.equals(isPrefix)) ? 1 : 0;
