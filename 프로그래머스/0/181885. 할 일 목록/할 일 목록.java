018e5ff196eb9e65974ea287e0d0dba88629b9eb
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-16 19:23:53 +0900

    Update 할 일 목록.java

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
         ArrayList<String> answer = new ArrayList<>();
-        int size = 0;
         
         for(int i = 0; i < todo_list.length; i++) {
             if(!finished[i]) {
@@ -14,3 +13,27 @@ class Solution {
         return answer.toArray(new String[answer.size()]);
     }
 }
+
+// 다른 풀이
+String str = "";
+
+for(int i=0; i<finished.length; i++){
+    str = finished[i]==false ? str+todo_list[i]+"," : str;
+}
+
+return str.split(",");
+
+// 다른 풀이
+return IntStream.range(0, todoList.length)
+                .mapToObj(i -> new AbstractMap.SimpleEntry<>(todoList[i], !finished[i]))
+                .filter(AbstractMap.SimpleEntry::getValue)
+                .map(AbstractMap.SimpleEntry::getKey)
+                .toArray(String[]::new);
+
+// 다른 풀이
+IntStream.range(0, finished.length)
+         .filter(i -> !finished[i])
+         .mapToObj(i -> todo_list[i])
+         .toArray(String[]::new);
+
+
