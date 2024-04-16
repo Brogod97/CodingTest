75565da610cdba8054d902d9beff8731ceb6f4fa
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-16 19:29:12 +0900

    Update 할 일 목록.py

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -6,3 +6,11 @@ def solution(todo_list, finished):
             answer.append(todo_list[i])
     
     return answer
+
+# 다른 풀이
+return [work for idx, work in enumerate(todo_list) if not finished[idx]]
+
+# 다른 풀이
+return [x for x, b in zip(todo_list, finished) if not b] 
+# zip(*iterables) : 두 개 이상의 시퀀스(리스트, 튜플 등)를 인자로 받음
+#                   각 시퀀스에서 같은 인덱스에 있는 요소들을 묶어서 튜플의 형태로 반환
