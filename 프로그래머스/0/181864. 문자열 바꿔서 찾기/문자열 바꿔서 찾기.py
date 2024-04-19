dba308d5c7ddab1b5bce61d5bc20eca02acb9f5b
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-19 22:01:07 +0900

    Update 문자열 바꿔서 찾기.py

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -3,3 +3,7 @@ def solution(myString, pat):
     newStr = myString.replace('A', 'a').replace('B', 'A').replace('a', 'B')
     
     return 1 if pat in newStr else 0
+
+# 다른 풀이
+return int(''.join(['A' if i == 'B' else 'B' for i in pat]) in myString)
+# pat의 길이가 더 짧기 때문에 pat의 값을 반전시키는 방법
