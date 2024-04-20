83418db65a54150005974aa2db11d80604b04c58
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-20 14:17:05 +0900

    Update A 강조하기.py

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,3 +1,14 @@
 def solution(myString):
     
     return myString.lower().replace('a','A')
+
+# 다른 풀이
+answer = ''
+for i in myString:
+    if i == 'a' or i == 'A':
+        answer += 'A'
+    elif i.isupper():
+        answer += i.lower()
+    else:
+        answer += i
+return answer
