13e63f4e6bd148b49d900d93b9ab40853f9c13e5
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-20 14:13:43 +0900

    Update A 강조하기.java

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -4,3 +4,8 @@ class Solution {
         return myString.toLowerCase().replaceAll("a", "A");
     }
 }
+
+// 다른 풀이
+return myString.chars()
+               .mapToObj(c -> String.valueOf((char) (c == 97 || c == 65 ? Character.toUpperCase(c) : Character.toLowerCase(c))))
+               .collect(Collectors.joining());
