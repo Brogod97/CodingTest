7796260bff9041aaa375cb2e71d0b7a83f35604a
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-21 12:40:29 +0900

    Update 원하는 문자열 찾기.java

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -4,3 +4,13 @@ class Solution {
         return myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
     }
 }
+
+// 다른 풀이
+int answer = 0;
+String str = myString.toLowerCase();
+String str2 = pat.toLowerCase();
+
+if (str.indexOf(str2) != -1) {
+    return 1;
+}
+return 0;
