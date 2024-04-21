6fb873f77d5b6adf8de43b9b9795751f18d7bf79
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-21 12:50:30 +0900

    Update 원하는 문자열 찾기.py

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -2,3 +2,9 @@ def solution(myString, pat):
     a = "a"
     
     return 1 if pat.lower() in myString.lower() else 0
+
+# 다른 풀이
+return int(pat.lower() in myString.lower())
+
+# 다른 풀이
+answer = myString.lower().find(pat.lower())
