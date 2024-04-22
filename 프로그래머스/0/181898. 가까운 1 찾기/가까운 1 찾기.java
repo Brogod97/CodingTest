479f5df036c7faf4f3cfa122bf918cf1f728ae36
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-22 21:39:01 +0900

    Update 가까운 1 찾기.java

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(int[] arr, int idx) {
         int answer = -1;
-        boolean flag = false;
         
         for(int i = idx; i < arr.length; i++) {
             if(arr[i] == 1) {
@@ -13,3 +12,9 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return IntStream.range(idx, arr.length)
+                .filter(i -> arr[i] == 1)
+                .findFirst()
+                .orElse(-1);
