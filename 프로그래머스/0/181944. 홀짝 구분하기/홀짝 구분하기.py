e16f6b5dd6a780b678061ff7ce336b10dc4bf17a
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-23 21:56:34 +0900

    Update 홀짝 구분하기.py

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -1,3 +1,9 @@
 a = int(input())
 
 print(str(a) + " is " + ("even" if a % 2 == 0 else "odd"))
+
+# 다른 풀이
+print(f"{n} is {'eovdedn'[n&1::2]}")
+
+# 다른 풀이
+print(f"{n} is {'even' if n % 2 == 0 else 'odd'}")
