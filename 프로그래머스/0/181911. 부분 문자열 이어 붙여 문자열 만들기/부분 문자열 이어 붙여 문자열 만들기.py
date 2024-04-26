212f47c6ee1b32388d10afb5756e3dc1a344ef99
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-26 19:04:41 +0900

    Update 부분 문자열 이어 붙여 문자열 만들기.py

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -1,8 +1,24 @@
 def solution(my_strings, parts):
     answer = ''
     cnt = 0
+    
     for string in my_strings:
         answer += string[parts[cnt][0] : parts[cnt][1] + 1]
         cnt += 1
     
     return answer
+
+# 다른 풀이
+answer = ""
+for i, (s, e) in enumerate(parts):
+    answer += my_strings[i][s:e+1]
+return answer
+
+# 다른 풀이
+return ''.join([x[y[0]:y[1]+1] for x,y in zip(my_strings, parts)])
+
+# 다른 풀이
+answer = ''
+for s, (x, y) in zip(my_strings, parts):
+    answer += s[x:y+1]
+return answer
