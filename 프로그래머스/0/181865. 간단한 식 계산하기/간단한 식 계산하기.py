7825631ff484d6ca45c2af3a478aa3eeb3a41cb6
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-27 18:37:55 +0900

    Update 간단한 식 계산하기.py

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,3 +1,9 @@
 def solution(binomial):
 
     return eval(binomial)
+
+# 다른 풀이
+a, op, b = binomial.split()
+if op == '+': return int(a) + int(b)
+if op == '-': return int(a) - int(b)
+if op == '*': return int(a) * int(b)
