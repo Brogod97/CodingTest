ee3e72eda567e7c20d4aff9ea0afdf5ab940a6e8
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-04-28 16:29:29 +0900

    Update 주사위 게임 1.py

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -2,10 +2,13 @@ def solution(a, b):
     answer = 0
     
     if(a % 2 == 1 and b % 2 == 1):
-        answer = pow(a, 2) + pow(b, 2)
+        answer = pow(a, 2) + pow(b, 2) # a ** 2 + b ** 2
     elif(a % 2 == 1 or b % 2 == 1):
         answer = 2 * (a + b)
     else:
         answer = abs(a - b)
     
     return answer
+
+# 다른 풀이
+return a*a+b*b if a & b & 1 else (a + b) << 1 if (a | b) & 1 else abs(a - b)
