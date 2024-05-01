692036ce86a9c3ed3394eb4da16a0256ff138f82
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-01 19:50:50 +0900

    Update l로 만들기.py

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -8,3 +8,11 @@ def solution(myString):
             answer += str
     
     return answer
+
+# 다른 풀이
+return myString.translate(str.maketrans('abcdefghijk', 'lllllllllll'))
+# str.replace(old, new) : 모든 old 문자열을 new 문자열로 치환
+
+# 다른 풀이
+answer = [x if x > 'l' else 'l' for x in myString]
+return ''.join(answer)
