c36a79da0f32998e1f758d5b18a70fe7cc4b8e03
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-04 21:05:41 +0900

    Update 369게임.py

--- 프로그래머스/0/120891. 369게임/369게임.py
+++ 프로그래머스/0/120891. 369게임/369게임.py
@@ -6,3 +6,14 @@ def solution(order):
             answer += 1
     
     return answer
+
+# 다른 풀이
+return sum(map(lambda x: str(order).count(str(x)), [3, 6, 9]))
+
+# 다른 풀이
+answer = 0
+order = str(order)
+return order.count('3') + order.count('6') + order.count('9')
+
+# 다른 풀이
+answer = len([1 for ch in str(order) if ch in "369"])
