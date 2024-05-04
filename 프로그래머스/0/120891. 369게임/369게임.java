646cce06ba3ccc72d4421b8628725ca919493f9c
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-04 21:00:36 +0900

    Update 369게임.java

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -12,3 +12,33 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+ int answer = 0;
+int count = 0;
+while(order != 0)
+{
+    if(order % 10 == 3 || order % 10 == 6 || order % 10 == 9)
+    {
+        count++;
+    }
+     order = order/10;
+}
+answer = count;
+return answer;
+
+// 다른 풀이
+return (int) Arrays.stream(String.valueOf(order).split(""))
+                   .map(Integer::parseInt)
+                   .filter(i -> i == 3 || i == 6 || i == 9)
+                   .count();
+
+// 다른 풀이
+private static final Pattern PATTERN = Pattern.compile("[369]");
+
+public int solution(int order) {
+    Matcher matcher = PATTERN.matcher(String.valueOf(order));
+    return (int) Stream.generate(() -> "")
+            .takeWhile(__ -> matcher.find())
+            .count();
+}
