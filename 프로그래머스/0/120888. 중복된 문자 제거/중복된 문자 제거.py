d21c454a2a9d0153c493158b75bd46c7c47f8203
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-06 19:39:48 +0900

    Update 중복된 문자 제거.py

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,4 +1,17 @@
 def solution(my_string):
     
-    return ''.join(dict.fromkeys(my_string)) # 파이썬 3.6부터 dict가 순서 보장
-    
+    return ''.join(dict.fromkeys(my_string)) 
+# dict.fromkeys()
+#     - 주어진 iterable(반복 가능한 객체)의 각 요소를 키로 하고, 모든 키에 대해 동일한 값으로 초기화된 딕셔너리를 생성
+#     - 모든 키에 대해 값으로 None을 가지는 딕셔너리를 생성
+# dict.fromkeys(my_string, initial_value) : 모든 키에 대해 초기값을 가지는 딕셔너리 생성
+
+# 파이썬 3.6부터 dict가 순서 보장
+# 명시적으론 collections.OrderedDict 사용
+
+# 다른 풀이
+answer = ''
+for i in my_string:
+    if i not in answer:
+        answer+=i
+return answer
