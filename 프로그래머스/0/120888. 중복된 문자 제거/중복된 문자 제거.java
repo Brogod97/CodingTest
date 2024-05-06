d656aa88ff04472ef42f5211e0eaaddede25f121
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-06 19:31:16 +0900

    Update 중복된 문자 제거.java

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -12,3 +12,31 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return my_string.chars()
+                .mapToObj(Character::toString)
+                .distinct()
+                .collect(Collectors.joining());
+
+// 다른 풀이
+String[] answer = my_string.split("");
+Set<String> set = new LinkedHashSet<String>(Arrays.asList(answer));
+
+return String.join("", set);
+
+// 다른 풀이
+String answer = "";
+
+for(int i=0; i<my_string.length(); i++){
+    //my_string.indexOf(my_string.charAt(i));
+    if(i==my_string.indexOf(my_string.charAt(i)))
+        answer+=my_string.charAt(i);
+}
+
+return answer;
+
+// 다른 풀이
+return Arrays.stream(myString.split(""))
+             .distinct()
+             .collect(Collectors.joining());
