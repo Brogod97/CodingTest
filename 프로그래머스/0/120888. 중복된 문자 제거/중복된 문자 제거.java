58c9c4dbc2e87726309fec86aaa2c22606f2664b
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-06 19:27:13 +0900

    [level 0] Title: 중복된 문자 제거, Time: 1.46 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        for(String str : my_string.split("")){
+            if(answer.contains(str)) {
+                continue;
+            }
+            answer += str;
+        }
+        
+        return answer;
+    }
+}
