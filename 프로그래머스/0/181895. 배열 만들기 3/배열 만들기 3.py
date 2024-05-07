f54ab621042990be8306addd044eb9d7fb28b251
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-07 18:31:39 +0900

    Update 배열 만들기 3.py

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -1,3 +1,13 @@
 def solution(arr, intervals):
 
     return arr[intervals[0][0]:intervals[0][1] + 1] + arr[intervals[1][0]:intervals[1][1] + 1]
+
+# 다른 풀이
+s1, e1 = intervals[0]
+s2, e2 = intervals[1]
+return arr[s1:e1+1] + arr[s2:e2+1]
+
+# 다른 풀이
+answer = []
+for a,b in intervals: answer+=arr[a:b+1]
+return answer
