efed0e8ef8721bda6fc19dd2e1fe49a27129bb0e
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-09 23:14:58 +0900

    Update 9로 나눈 나머지.java

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -9,3 +9,16 @@ class Solution {
         return answer % 9;
     }
 }
+
+// 다른 풀이
+return number.chars()
+             .map(c -> (c - '0') % 9)
+             .sum() % 9;
+
+// 다른 풀이
+BigInteger x = new BigInteger(number);
+BigInteger y = new BigInteger("9");
+String ans = "";
+ans += x.remainder(y);
+int answer = Integer.parseInt(ans);
+return answer;
