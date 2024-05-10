ec106984e22fceca77a67ebd1947c5a6a596111e
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-10 15:28:26 +0900

    Update 주사위 게임 2.java

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -17,3 +17,34 @@ class Solution {
         return (int)(Math.pow(a , power) + Math.pow(b , power) + Math.pow(c , power));
     }
 }
+
+// 다른 풀이
+public int solution(int a, int b, int c) {
+    int answer = 1;
+
+    int count = 1;
+    if(a == b || a == c || b == c) {
+        count++;
+    }
+
+    if(a == b && b == c) {
+        count++;
+    }
+
+    for(int i = 1; i <= count; i++) {
+        answer *= (pow(a,i)+pow(b,i)+pow(c,i));
+    }
+
+    return answer;
+}
+
+private int pow(int a, int b) {
+    if(b == 0) return 1;
+    return a * pow(a, b-1);
+}
+
+// 다른 풀이
+Set<Integer> numbers = Stream.of(a, b, c).collect(Collectors.toSet());
+return (a + b + c) *
+        (numbers.size() < 3 ? a * a + b * b + c * c : 1) *
+        (numbers.size() < 2 ? a * a * a + b * b * b + c * c * c : 1);
