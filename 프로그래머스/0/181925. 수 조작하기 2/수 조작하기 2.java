75efd3b4428a192ae1f3dcb2687f5b939bc99c78
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-12 19:51:22 +0900

    Update 수 조작하기 2.java

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -14,3 +14,16 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+ String answer = "";
+for(int i=1; i<numLog.length; i++){
+    int j = numLog[i-1] - numLog[i];
+    switch(j){
+        case -1 : answer+='w'; break;
+        case 1 : answer+='s'; break;
+        case -10 : answer+='d'; break;
+        case 10 : answer+='a'; break;
+    }
+}
+return answer;
