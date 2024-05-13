3522795eaab5bbd2541a68fc0e7848f130ab98f6
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-13 18:47:18 +0900

    Update 커피 심부름.java

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -15,3 +15,24 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+int answer = 0;
+for(String o : order) {
+    if(o.contains("cafelatte")) {
+        answer += 5000;
+    } else {
+        answer += 4500;
+    }
+}
+return answer;
+
+// 다른 풀이
+int answer = 0;
+for (String eachOrder : order) {
+    if (eachOrder.contains("americano") || eachOrder.equals("anything"))
+        answer += 4500;
+    else 
+        answer += 5000;
+}
+return answer;
