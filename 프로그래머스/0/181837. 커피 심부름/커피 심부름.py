f2f470debe256d2fb9de165766f8acc654dd6d43
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-13 18:50:21 +0900

    Update 커피 심부름.py

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -8,3 +8,15 @@ def solution(order):
             answer += 5000
     
     return answer
+
+# 다른 풀이
+answer = 0
+for want in order:
+    if 'latte' in want:
+        answer += 500
+    answer += 4500
+
+return answer
+
+# 다른 풀이
+return sum([5000 if "cafelatte" in i else 4500 for i in order])
