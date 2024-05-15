33d37acf0ac4697f9326fc96b03e5d149dd9e2b8
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-15 18:17:24 +0900

    Update 1로 만들기.py

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -11,3 +11,17 @@ def solution(num_list):
                 answer += 1
     
     return answer
+
+# 다른 풀이
+return sum(len(bin(i)) - 3 for i in num_list)
+
+# 다른 풀이
+ answer = 0
+
+for n in num_list:
+    while n != 1:
+        n //= 2
+        answer += 1
+
+return answer
+
