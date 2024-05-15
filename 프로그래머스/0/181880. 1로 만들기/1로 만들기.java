9a893e0bff602607373e977a50c0f78d7024da26
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-15 18:11:49 +0900

    Update 1로 만들기.java

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -4,7 +4,7 @@ class Solution {
         
         for(int num : num_list) {
             while(num != 1){
-                num = num % 2 == 0 ? num / 2 : (num - 1) / 2;
+                num = num % 2 == 0 ? num / 2 : (num - 1) / 2; // 참고: int 타입이라 / 연산 시 -1을 안해도 절삭됨
                 answer++;
             }
         }
@@ -12,3 +12,8 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return Arrays.stream(num_list)
+             .map(i -> Integer.toBinaryString(i).length() - 1)
+             .sum();
