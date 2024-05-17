b005bf808917935365dfb2f92bc2117b395a4257
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-17 22:13:33 +0900

    Update 문자열 내 p와 y의 개수.java

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -3,14 +3,52 @@ class Solution {
         int p = 0;
         int y = 0;
         
+        s..toLowerCase();
         for(String str : s.split("")) {
-            if(str.toLowerCase().equals("p")) {
+            if(str.equals("p")) {
                 p++;
-            } else if(str.toLowerCase().equals("y")) {
-              y++;  
+            } else if(str.equals("y")) {
+                y++;  
             }
         }
 
         return p == y ? true : false;
     }
 }
+
+// 다른 풀이
+s = s.toUpperCase();
+return s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
+
+// 다른 풀이
+s = s.toLowerCase();
+    int count = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+
+        if (s.charAt(i) == 'p')
+            count++;
+        else if (s.charAt(i) == 'y')
+            count--;
+    }
+
+    if (count == 0)
+        return true;
+    else
+        return false;
+}
+
+// 다른 풀이
+return s.replaceAll("[^yY]", "").length() - s.replaceAll("[^pP]", "").length() == 0 ? true : false;
+
+
+
+
+
+
+
+
+
+
+
+
