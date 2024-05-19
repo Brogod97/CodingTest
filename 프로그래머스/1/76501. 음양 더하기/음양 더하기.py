61a57e303c893161614ecf6a934a42d88edfd3f6
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-19 15:10:18 +0900

    Update 음양 더하기.py

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -8,3 +8,8 @@ def solution(absolutes, signs):
             answer -= absolutes[i]
             
     return answer
+
+# 다른 풀이
+return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
+# zip(*iterables)
+#     - 각 iterable에서 동일한 위치에 있는 요소들을 튜플로 묶어주는 역할
