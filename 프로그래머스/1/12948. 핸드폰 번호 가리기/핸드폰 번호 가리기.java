c90293eaa8b098872c2ff4c113835beba05b7bd6
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-21 12:17:14 +0900

    Update 핸드폰 번호 가리기.java

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -11,3 +11,13 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+ char[] ch = phone_number.toCharArray();
+ for(int i = 0; i < ch.length - 4; i ++){
+     ch[i] = '*';
+ }
+ return String.valueOf(ch);
+
+// 다른 풀이
+return phone_number.replaceAll(".(?=.{4})", "*");
