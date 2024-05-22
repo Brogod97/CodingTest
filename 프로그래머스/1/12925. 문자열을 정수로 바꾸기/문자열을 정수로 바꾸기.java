fe667c7d6a6d15b0f99b64e480afc81f3195606f
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-22 12:35:40 +0900

    Update 문자열을 정수로 바꾸기.java

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -3,3 +3,16 @@ class Solution {
         return Integer.parseInt(s);
     }
 }
+
+// 다른 풀이
+boolean Sign = true;
+int result = 0;
+
+for (int i = 0; i < str.length(); i++) {
+    char ch = str.charAt(i);
+    if (ch == '-')
+        Sign = false;
+    else if(ch !='+')
+        result = result * 10 + (ch - '0');
+}
+return Sign?1:-1 * result;
