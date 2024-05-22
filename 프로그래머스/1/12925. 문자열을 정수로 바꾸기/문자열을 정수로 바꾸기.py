ce01c82e88192ecb1664189c7ec51281ce0884f0
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-22 12:36:44 +0900

    Update 문자열을 정수로 바꾸기.py

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -1,2 +1,11 @@
 def solution(s):
     return int(s)
+
+# 다른 풀이
+result = 0
+for idx, number in enumerate(str[::-1]):
+    if number == '-':
+        result *= -1
+    else:
+        result += int(number) * (10 ** idx)
+return result
