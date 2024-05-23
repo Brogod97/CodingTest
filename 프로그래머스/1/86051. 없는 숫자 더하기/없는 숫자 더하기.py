f30d6cdd5afe89f27e57c89446ce67d0134b5d11
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-23 12:30:35 +0900

    Update 없는 숫자 더하기.py

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,3 +1,16 @@
 def solution(numbers):
     
     return 45 - sum(numbers)
+
+# 다른 풀이
+return sum([i for i in [1,2,3,4,5,6,7,8,9,0] if i not in numbers])
+
+# 다른 풀이
+answer=0
+for i in range(1,10):
+    if i not in numbers:
+        answer += i
+return answer
+
+# 다른 풀이
+return sum(set(range(10)) - set(numbers))
