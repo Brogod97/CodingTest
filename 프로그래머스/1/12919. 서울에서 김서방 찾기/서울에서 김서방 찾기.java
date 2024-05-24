c09f4f19297ca730a0743185c04a475cad55077c
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-24 12:12:04 +0900

    Update 서울에서 김서방 찾기.java

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -6,3 +6,12 @@ class Solution {
         return "김서방은 " + Arrays.asList(seoul).indexOf("Kim") + "에 있다";
     }
 }
+
+// 다른 풀이
+for(int i=0; i<seoul.length; i++){
+    if(seoul[i].equals("Kim")) {
+        x = i ;
+        break;
+    }
+ }
+return "김서방은 "+ x + "에 있다";
