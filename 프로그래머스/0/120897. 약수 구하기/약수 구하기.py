d1368ef2b19568309d81f564db628410a7aaad07
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-25 14:13:58 +0900

    Update 약수 구하기.py

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,3 +1,11 @@
 def solution(n):
     
     return [i for i in range(1, n + 1) if n % i == 0]
+
+# 다른 풀이
+return list(filter(lambda v: n % v == 0, [i for i in range(1, n//2+1)])) + [n]
+
+# 다른 풀이
+for i in range(1, n + 1):
+    if n % i == 0:
+        answer.append(i)
