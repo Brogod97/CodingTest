efc0ac0710acfc925df374d63fd2a00d85637374
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-27 12:33:35 +0900

    Update 두 정수 사이의 합.java

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -15,3 +15,19 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+ public long solution(int a, int b) {
+    return sumAtoB(Math.min(a, b), Math.max(b, a));
+}
+
+private long sumAtoB(long a, long b) {
+    return (b - a + 1) * (a + b) / 2;
+}
+
+// 다른 풀이
+long answer = 0;
+for (int i = ((a < b) ? a : b); i <= ((a < b) ? b : a); i++) 
+  answer += i;
+
+return answer;
