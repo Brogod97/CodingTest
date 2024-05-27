9ebaff83cb6c8c83612db6d9fc5c6a96ff6a80bf
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-27 12:36:33 +0900

    Update 두 정수 사이의 합.py

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -1,3 +1,14 @@
 def solution(a, b):
     
     return sum([i for i in range(a, b + 1)] if a < b else [i for i in range(b, a + 1)])
+
+# 다른 풀이
+if a > b:
+    a, b = b, a
+return sum(range(a, b + 1))
+
+# 다른 풀이
+return (abs(a-b)+1)*(a+b)//2
+
+# 다른 풀이
+return sum(range(min(a, b), max(a, b)+1))
