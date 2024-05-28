602aee956c718dc26eca4b0376cbdf2356fa4ddf
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-28 12:22:42 +0900

    Update 하샤드 수.java

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -3,9 +3,13 @@ class Solution {
         int sum = 0;
         
         for(String num : Integer.toString(x).split("")) {
-            sum += Integer.valueOf(num);
+            sum += Integer.parseInt(num);
         }
         
-        return x % sum == 0 ? true: false;
+        return x % sum == 0 ? true: false; // 애초에 boolean 타입으로 반환됨
     }
 }
+
+// 다른 풀이 
+int sum = String.valueOf(x).chars().map(ch -> ch - '0').sum();
+return x % sum == 0;
