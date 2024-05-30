d9474f910ef8185ca9c1709cd4fb50cb7659d5cb
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-30 12:25:01 +0900

    Update 문자열 다루기 기본.py

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,2 +1,15 @@
 def solution(s):
     return s.isdigit() and (len(s) == 4 or len(s) == 6)
+# isdigit(): 문자열이 숫자로만 이루어져 있는지 확인
+
+
+# 다른 풀이
+return s.isdigit() and len(s) in [4,6]
+
+# 다른 풀이
+try:
+    int(s)
+except:
+    return False
+return len(s) == 4 or len(s) == 6
+
