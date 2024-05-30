c8c1efb3bd33b212027aed3b7a8afefe44c201f8
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-05-30 12:22:02 +0900

    Update 문자열 다루기 기본.java

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -15,3 +15,19 @@ class Solution {
         return  answer;
     }
 }
+
+// 다른 풀이
+ if(s.length() == 4 || s.length() == 6){
+  try{
+      int x = Integer.parseInt(s);
+      return true;
+  } catch(NumberFormatException e){
+      return false;
+  }
+}
+else return false;
+
+// 다른 풀이
+if (s.length() == 4 || s.length() == 6) return s.matches("(^[0-9]*$)");
+return false;
+
