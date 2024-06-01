9ec8b91848918ccf2762a8cfe95310d72c2b2a6b
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-06-01 19:55:23 +0900

    Update 수박수박수박수박수박수？.py

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -8,3 +8,13 @@ def solution(n):
             answer += "박"
     
     return answer
+
+# 다른 풀이
+str = "수박"*n
+return str[:n]
+
+# 다른 풀이
+return "수박" * (n//2) + "수" * (n%2)
+
+# 다른 풀이
+return "".join(["수박"[i%2] for i in range(n)])
