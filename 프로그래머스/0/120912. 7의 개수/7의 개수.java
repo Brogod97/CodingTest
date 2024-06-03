54714ba439336be591b129cbcc4a2771ba5eb7b5
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-06-03 12:16:49 +0900

    Update 7의 개수.java

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -11,3 +11,25 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return (int) Arrays.stream(
+                Arrays.stream(array)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining())
+                        .split("")
+        )
+        .filter(s -> s.equals("7"))
+        .count();
+
+// 다른 풀이
+int answer = 0;
+for(int a : array){
+    while(a != 0){
+        if(a % 10 == 7){
+            answer++;
+        }
+        a /= 10;
+    }
+}
+return answer;
