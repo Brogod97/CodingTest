6184018c622ddd51c57f363821ae6afec24896cd
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-06-09 14:08:56 +0900

    Update 문자열 겹쳐쓰기.java

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -9,3 +9,16 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+String before = my_string.substring(0, s);
+String after = my_string.substring(s + overwrite_string.length());
+return before + overwrite_string + after;
+
+// 다른 풀이
+char[] my_chars = my_string.toCharArray();
+char[] overwrite_chars = overwrite_string.toCharArray();
+for (int i=0; i<overwrite_chars.length; i++) {
+    my_chars[s + i] = overwrite_chars[i];
+}
+return String.valueOf(my_chars);
