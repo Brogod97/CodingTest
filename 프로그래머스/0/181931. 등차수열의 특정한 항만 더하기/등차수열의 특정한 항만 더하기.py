21ea08d3d09d1435cbcaa93ffd45a738ae4f8e13
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-06-11 12:30:25 +0900

    Update 등차수열의 특정한 항만 더하기.py

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -6,3 +6,12 @@ def solution(a, d, included):
             answer += a + d * i
     
     return answer
+    
+# 다른 풀이
+answer = 0
+for i in range(len(included)):
+    answer += (a + d * i) * int(included[i])
+return answer
+
+# 다른 풀이
+return sum(a + i * d for i, f in enumerate(included) if f)
