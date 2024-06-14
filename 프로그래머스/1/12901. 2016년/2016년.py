e570af8087f9f9cbae20a490f8f0e9a6a0ad5d41
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-06-14 12:34:30 +0900

    Update 2016년.py

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -6,3 +6,12 @@ def solution(a, b):
     day = datetime.date(2016, a, b).weekday()
     
     return week[day]
+
+# 다른 풀이
+month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+day = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+return day[(sum(month[:a-1])+b-1)%7]
+
+# 다른 풀이
+t = 'MON TUE WED THU FRI SAT SUN'.split()
+return t[datetime.datetime(2016, a, b).weekday()]
