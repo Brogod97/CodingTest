0371113f03830d64155825097384bab6a963baf1
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-06-16 23:39:03 +0900

    Update 코드 처리하기.java

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -25,3 +25,19 @@ class Solution {
         return ret.equals("") ? "EMPTY" : ret;
     }
 }
+
+// 다른 풀이
+StringBuilder answer = new StringBuilder();
+int mode = 0;
+for (int i = 0; i < code.length(); i++) {
+    char current = code.charAt(i);
+    if (current == '1') {
+        mode = mode == 0 ? 1 : 0;
+        continue;
+    }
+
+    if (i % 2 == mode) {
+        answer.append(current);
+    }
+}
+return answer.length() == 0 ? "EMPTY" : answer.toString();
