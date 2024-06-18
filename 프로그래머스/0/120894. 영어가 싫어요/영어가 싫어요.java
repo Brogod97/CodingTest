1f688f064b71940a82a12467dc8bd157e5d440c9
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-06-18 12:24:12 +0900

    [level 0] Title: 영어가 싫어요, Time: 7.60 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(String numbers) {
+        String[] numStr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i = 0; i < numStr.length; i++) {
+            numbers = numbers.replaceAll(numStr[i], i + "");
+        }
+        
+        return Long.parseLong(numbers);
+    }
+}
