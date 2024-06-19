b39851427bec66a98829628aa5a627fd1727cd66
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-06-19 12:18:32 +0900

    Update 문자열 뒤집기.java

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -7,3 +7,18 @@ class Solution {
         return my_string.substring(0, s) + reversed + my_string.substring(e + 1, my_string.length());
     }
 }
+
+// 다른 풀이
+StringBuilder answer = new StringBuilder(my_string.substring(s, e + 1));
+answer.reverse();
+return my_string.substring(0, s) + answer + my_string.substring(e + 1);
+
+// 다른 풀이
+char[] arr = my_string.toCharArray();
+while (s < e) {
+    char temp = arr[s];
+    arr[s++] = arr[e];
+    arr[e--] = temp;
+}
+
+return new String(arr);
