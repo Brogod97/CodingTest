d99d78bfa9d1905216f5f19af8c0048629c0103e
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-07-07 21:18:41 +0900

    Update 진료 순서 정하기.java

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -18,3 +18,23 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return Arrays.stream(e).map(i -> Arrays.stream(e).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList()).indexOf(i) + 1).toArray();
+
+// 다른 풀이
+int[] answer = new int[emergency.length];
+
+for(int i = 0; i < answer.length; i++){
+    if(answer[i] != 0){
+        continue;
+    }
+    int idx = 1;
+    for(int j = 0; j < answer.length; j++){
+        if(emergency[i] < emergency[j]){
+            idx++;
+        }
+    }
+    answer[i] = idx;
+}
+return answer;
