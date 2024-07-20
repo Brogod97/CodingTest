b13baf758a5e5084bce102e52b760e2ed1d3d3de
Cho Hyeong shin <90192797+Brogod97@users.noreply.github.com>
2024-07-20 23:10:40 +0900

    Update 한 번만 등장한 문자.java

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -19,3 +19,27 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+return Arrays.stream(s.split(""))
+                .collect(Collectors.groupingBy(s1 -> s1))
+                .entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().size() <= 1)
+                .map(Map.Entry::getKey)
+                .sorted()
+                .collect(Collectors.joining());
+
+// 다른 풀이
+int[] alpha = new int[26];
+for(char c : s.toCharArray()){
+    alpha[c - 'a']++;
+}
+
+StringBuilder answer = new StringBuilder();
+for(int i = 0; i < 26; i++){
+    if(alpha[i] == 1){
+        answer.append((char)(i + 'a'));
+    }
+}
+return answer.toString();
